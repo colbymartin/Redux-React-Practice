35f1dc40d2afb02ea7a844d54ae5e48ddda7289c
Colby Martin <colbymartin@Colbys-MacBook-Air.local>
2017-07-10 17:28:23 -0400

    Reducers and Actions on Book App

---
+++ ReduxSimpleStarter-master/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 809},
+        { title: "The Dark Tower", pages: 1209},
+        { title: "Born Standing Up", pages: 380},
+    ]
+}
